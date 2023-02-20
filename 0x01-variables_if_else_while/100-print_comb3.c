0ea9c120783cf19cc0dab05afb827a9b22a53dd5
genthamid <abdulhamidimam1226@gmail.com>
2023-02-20 14:50:08 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function
+ * Description: prints two digits
+ * Return: always o
+ */
+int main(void)
+{
+	int c, i;
+	for (c = 'o' ; c <= '9' ; c++)
+	{
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
