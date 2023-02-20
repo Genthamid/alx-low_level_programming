073c13cdc8bd830f2757cc977277afc8c207a3d5
genthamid <abdulhamidimam1226@gmail.com>
2023-02-19 23:36:06 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stio.h>
+
 /**
  * main - this is the main function
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
